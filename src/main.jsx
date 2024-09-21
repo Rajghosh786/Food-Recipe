5a09a304e9f042b1f72d9472252d2abe72aa5ca5
Raj <rajghosh786@gmail.com>
2024-09-21 18:44:36 +0530

    trying with hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,52 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { createBrowserRouter,Route, RouterProvider, createRoutesFromElements} from 'react-router-dom'
-import Layout from './Layout.jsx'
-import Home from './components/Home/Home.jsx'
-import Favourites from './components/Favourites/Favourites.jsx'
-import Contact from './components/Contact/Contact.jsx'
-import About from './components/About/About.jsx'
-import Login from './components/Login/Login.jsx'
-import Register from './components/Register/Register.jsx'
-import FullRecipe from './components/Home/FullRecipe.jsx'
-import UserContextProvider from './components/context/UserContextProvider.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+import Layout from './Layout.jsx';
+import Home from './components/Home/Home.jsx';
+import Favourites from './components/Favourites/Favourites.jsx';
+import Contact from './components/Contact/Contact.jsx';
+import About from './components/About/About.jsx';
+import Login from './components/Login/Login.jsx';
+import Register from './components/Register/Register.jsx';
+import FullRecipe from './components/Home/FullRecipe.jsx';
+import UserContextProvider from './components/context/UserContextProvider.jsx';
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <UserContextProvider>
+      <HashRouter>
+        <Layout>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="fullrecipe/:id" element={<FullRecipe />} />
+            <Route path="favourites" element={<Favourites />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="about" element={<About />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+          </Routes>
+        </Layout>
+      </HashRouter>
+    </UserContextProvider>
+  </StrictMode>
+);
+
+// import { StrictMode } from 'react'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.jsx'
+// import './index.css'
+// import { createBrowserRouter,Route, RouterProvider, createRoutesFromElements} from 'react-router-dom'
+// import Layout from './Layout.jsx'
+// import Home from './components/Home/Home.jsx'
+// import Favourites from './components/Favourites/Favourites.jsx'
+// import Contact from './components/Contact/Contact.jsx'
+// import About from './components/About/About.jsx'
+// import Login from './components/Login/Login.jsx'
+// import Register from './components/Register/Register.jsx'
+// import FullRecipe from './components/Home/FullRecipe.jsx'
+// import UserContextProvider from './components/context/UserContextProvider.jsx'
+// import { HashRouter } from 'react-router-dom'
 
 // const router = createBrowserRouter([
 //   {
@@ -38,26 +73,26 @@ import UserContextProvider from './components/context/UserContextProvider.jsx'
 //   }
 // ])
 
-const router = createBrowserRouter(
-  createRoutesFromElements(
-      <Route path='https://Rajghosh786/github.io/Food-Recipe' element={<Layout/>}>
-        <Route path='' element={<Home/>}/>
-        <Route path='Food-Recipe/fullrecipe/:id' element={<FullRecipe/>}/>
-        <Route path='Food-Recipe/favourites' element={<Favourites/>}/>
-        <Route path='Food-Recipe/contact' element={<Contact/>}/>
-        <Route path='Food-Recipe/about' element={<About/>}/>
-        <Route path='Food-Recipe/login' element={<Login/>}/>
-        <Route path='Food-Recipe/register' element={<Register/>}/>
-      </Route>
-  )
-)
+// const router = createBrowserRouter(
+//   createRoutesFromElements(
+//       <Route path='/' element={<Layout/>}>
+//         <Route path='' element={<Home/>}/>
+//         <Route path='fullrecipe/:id' element={<FullRecipe/>}/>
+//         <Route path='favourites' element={<Favourites/>}/>
+//         <Route path='contact' element={<Contact/>}/>
+//         <Route path='about' element={<About/>}/>
+//         <Route path='login' element={<Login/>}/>
+//         <Route path='register' element={<Register/>}/>
+//       </Route>
+//   )
+// )
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    {/* <App /> */}
-    <UserContextProvider>
-      <RouterProvider router={router}> 
-      </RouterProvider>
-      </UserContextProvider>
-  </StrictMode>,
-)
+// createRoot(document.getElementById('root')).render(
+//   <StrictMode>
+//     {/* <App /> */}
+//     <UserContextProvider>
+//       <RouterProvider router={router}> 
+//       </RouterProvider>
+//       </UserContextProvider>
+//   </StrictMode>,
+// )
